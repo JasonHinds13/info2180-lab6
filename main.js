6268771c16dc2100ebd4949d45153591b095536f
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2016-11-05 02:34:38 +0000

    Exercise 4

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    
+    //search button for specific
     $("#search").on('click', function(){
        var query = $("#query").val();
        var link = "https://info2180-lab6-jasonhinds13.c9users.io/request.php?q="+query;
@@ -11,4 +13,29 @@ $(document).ready(function(){
            $("#result").html(err);
        });
     });
+    
+    //search all button
+    $("#searchall").on('click', function(){
+        var link = "https://info2180-lab6-jasonhinds13.c9users.io/request.php?q=&all=true";
+        
+        $.ajax(link, {
+           method: 'GET',
+           dataType: "xml",
+       }).done(function(res){
+           var defs = $(res).find('definition');
+           
+           $("#result").html(""); //clear if previous result given
+           $("#result").append('<ol></ol>');
+           
+           $(defs).each(function(){
+               var ret = '<h3>'+$(this).attr('name')+'</h3>';
+               ret += '<p>'+$(this).text()+'</p>';
+               ret += '<p> -'+$(this).attr('author')+'</p>';
+               $("#result ol").append('<li>'+ret+'</li>');
+           });
+       }).fail(function(err){
+           $("#result").html(err);
+       });
+    });
+    
 });
